9ae2b4c1dd5e67c132bbc5d6b3eb1fb597e00ac8
Suhrobbek Soatov <svkhrobbeck@gmail.com>
2023-05-31 23:56:46 +0500

    Render films component inside home page component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { Films, Header } from "../../components";
 import HeroBanner from "../../components/hero-banner/HeroBanner";
+import request from "../../service/request";
 
 const Home = () => {
   const { loggedIn } = useSelector(state => state.auth);
@@ -13,7 +14,13 @@ const Home = () => {
           <Header />
           <main className="main-content">
             <HeroBanner />
-            <Films />
+            <Films title="NETFLIX ORIGINALS" isLarge={true} requestStr={request.NetflixOriginals} />
+            <Films title="Trending Now" requestStr={request.Trending} />
+            <Films title="Top Rated" requestStr={request.ActionMovies} />
+            <Films title="Action Movies" requestStr={request.ComedyMovies} />
+            <Films title="Comedy Movies" requestStr={request.HorrorMovies} />
+            <Films title="Romance Movies" requestStr={request.RomanceMovies} />
+            <Films title="Documentary Movies" requestStr={request.Documentaries} />
           </main>
         </>
       ) : (
